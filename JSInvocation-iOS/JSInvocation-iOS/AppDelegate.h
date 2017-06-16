ed17af6a52fe84546bbeaadffe54b1b4d5875770
/ <65301524@qq.com>
2017-06-16 14:23:24 +0800

    folder

---
+++ JSInvocation-iOS/JSInvocation-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSInvocation-iOS
+//
+//  Created by Awhisper on 2017/6/16.
+//  Copyright © 2017年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
